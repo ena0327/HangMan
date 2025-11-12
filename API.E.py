7c25875f92b1e211b5a86b162d050008dc545b37
ena0327 <enaclarke1999@gmail.com>
2025-11-12 13:49:41 -0500

    Update API.E.py

--- API.E.py
+++ API.E.py
@@ -4,4 +4,23 @@
 
 import requests 
 
+base_url = "https://pokeapi.co/api/v2/"
+def get_pokemon_info(name):
+  
+    url = f"{base_url}/pokemon/{name}"
+    response = requests.get(url)
+    print()
+
+    pokemon_name= "pikachu"
+    get_pokemon_info(pokemon_name)
+    
+
+
+
+
+
+
+
+
+
 
