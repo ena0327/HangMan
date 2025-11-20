8573aed15ec06312450fdb329cde0d81493e0989
ena0327 <enaclarke1999@gmail.com>
2025-11-20 14:11:15 -0500

    fudud
    dusu

--- API.Q.py
+++ API.Q.py
@@ -1,3 +1,13 @@
 import random
 
+pokemon_list = []
 
+import requests
+
+url = "https://cat-breeds.p.rapidapi.com/cat_breeds"
+
+headers = {"x-rapidapi-host": "cat-breeds.p.rapidapi.com"}
+
+response = requests.get(url, headers=headers)
+
+print(response.json())
