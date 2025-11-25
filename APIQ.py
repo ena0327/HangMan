0541003e3550398b42955b4c5f981e3038a11827
quafiazubayeer <quafiazubayeer26@gmail.com>
2025-11-25 14:53:42 -0500

    Update APIQ.py

--- APIQ.py
+++ APIQ.py
@@ -57,55 +57,66 @@ HANGMAN_STAGES = [
       |
 ========='''
 ]
+WORD_LIST = ["python", "hangman", "programming", "challenge"]
 
 def display_hangman(wrong_guesses):
     print(HANGMAN_STAGES[wrong_guesses])
 
-    def_current_word(word,guessed_letters):
-    displayed = ""
-    for letter in word :
-        if letter in guessed_letters :
-            display += letter+ " "
-      else:
-        display += "_"
-        print (display)
+def current_word(word,guessed_letters):
+  displayed = ""
+  for letter in word :
+    if letter in guessed_letters :
+      display += letter+ " "
+    else:
+      display += "_"
+      print (display)
+
+secret_word = ""
+guessed_letter = ""
+rong_guesse =""
+max_wrong_guesses = ""
 
 def play_hangman():
-#  secret_word
-guessed_letters =[]
-wrong_guesses = 0
-max_wrong_guesses = len(HANGMANSTAGES) - 1
+  global secret_word
+  secret_word = chosen_word(WORD_LIST)
+  global guessed_letter
+  guessed_letter= []
+  global wrong_guesses
+  wrong_guesses = 0
+  global max_wrong_guesses
+  max_wrong_guesses = len(HANGMANSTAGES) - 1
 
-print ( "game started")
+  print ( "game started")
 
-while wrong_guesses < max_wrong_guesses and "_" in display_game_state(secret_word,guessed_letters):
+  while wrong_guesses < max_wrong_guesses and "_" in display_game_state(secret_word,guessed_letters):
     display_hangman(wrong_guesses)
     display_game_state (secret_words , guessed_letters)
-    print(f"Guessed letters : {','. join (sorted (guessed letters))}") 
-
+    print(f"Guessed letters : {','. join (sorted(guessed letters))}")
     guess = input("guess a letter : ").lower()
 
     if not guess.isalpha() or len(guess) ! = 1:
       print("Please enter a single letter .")
-    continue
-   if guess in guessed_letters:
+      continue
+    if guess in guessed_letters:
       print(" You already guessed that letter")
-    continue
+      continue
 
+    guessed_letters.add(guess)
 
- guessed_letters.add(guess)
-
-if guess in chosen_word:
-    for i , char in enumarate(chosen_word) :
+    if guess in chosen_word:
+      for i , char in enumarate(chosen_word) :
         if char == guess
-    print("Correct guess!")
-  else :
-    incorrected_guess += 1
-    print(" Incorrect guess")
+        print("Correct guess!")
+      else :
+        incorrected_guess += 1
+        print(" Incorrect guess")
 
-  display_game_state(incorrect_guesses,hidden_word_display,guessed_letters)
+      display_game_state(incorrect_guesses,hidden_word_display,guessed_letters)
 
-  if"_" not in hidden_word_display :
-    print(f"You guessed the word :{chosen_word} ")
-  else :
-   print
+    if"_" not in hidden_word_display :
+      print(f"You guessed the word :{chosen_word} ")
+    else :
+      print(f" You ran out guesses ! the word was: {chosen_word}")
+
+      if _name_== "_main_" :
+        play_hangman():
