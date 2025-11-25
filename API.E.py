a999609ca211ec6814e430acdcd6262e9314fd5c
ena0327 <enaclarke1999@gmail.com>
2025-11-25 14:04:30 -0500

    Update API.E.py

--- API.E.py
+++ API.E.py
@@ -25,12 +25,8 @@ def get_pokemon_info(name):
         print(f"Error: {response.status_code}")     
         #hdgfhxskghdgfskgtksk QUAFIA HATES ME ;[
 
-<<<<<<< HEAD
-get_pokemon_info(" ")
-=======
         get_pokemon_info(" ")
         random = __import__('random')
 random_word = random.choice(pokeylist)
 
 print(f"The randomly chosen element is: {random_word}")
->>>>>>> 53a6671b4fe29ce94ba047d1fde62ad550bcd048
